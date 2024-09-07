b985b79ec352910337470aeaebd8a322374b0b0c
DoctorKrill08 <doctorkrill08@gmail.com>
2024-09-07 16:15:21 -0400

    Split Arm code from Mecanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpBasic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpBasic.java
@@ -3,23 +3,27 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Subsystem.Arm;
 import org.firstinspires.ftc.teamcode.Subsystem.DriveTrain;
 
 @TeleOp
-public class TeleOpBasic extends LinearOpMode {
+public class TeleOpBasic extends LinearOpMode{
+
     @Override
     public void runOpMode() throws InterruptedException {
         waitForStart();
         if (isStopRequested()) return;
         DriveTrain driveTrain = new DriveTrain();
+        Arm arm = new Arm();
         driveTrain.initiate(hardwareMap);
+        arm.initiate(hardwareMap);
         while (opModeIsActive()) {
             double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
             double x = gamepad1.left_stick_x * 1.1;
             double rx = gamepad1.right_stick_x;
             double slidepower = gamepad1.right_trigger - (gamepad1.left_trigger);
             driveTrain.move(x,y,rx);
-            driveTrain.arm(slidepower, telemetry);
+            arm.slides(slidepower,telemetry);
         }
     }
 }
